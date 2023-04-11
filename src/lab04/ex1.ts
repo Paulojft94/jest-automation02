212aa77ca237e11ac19080b7f3676e53fcb08504
Paulojft94 <1222013@isep.ipp.pt>
2023-04-12 00:41:38 +0100

    add first exercise lab04

---
+++ src/lab04/ex1.ts
@@ -0,0 +1,4 @@
+import { numAlgarismos } from "../lab03/lab03_funcoes/funcoes03";
+
+const input: number = 1234;
+console.log(numAlgarismos(input));
---
+++ src/lab04/lab04_funcoes/funcoes04.ts
@@ -0,0 +1,3 @@
+////Funcções do Lab04
+
+//Exercício 1: Contador de dígitos de um número (*)
